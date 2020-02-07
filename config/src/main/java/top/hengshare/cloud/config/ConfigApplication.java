cc769a1784769cc095e09e1c183c6e220f0d5673
StivenYang <StivenYang626@gmail.com>
2020-02-07 17:36:28 +0800

    提交配置中心

---
+++ config/src/main/java/top/hengshare/cloud/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package top.hengshare.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yang
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
